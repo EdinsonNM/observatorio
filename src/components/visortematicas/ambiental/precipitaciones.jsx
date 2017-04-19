65d293e416b1441a707da6acb0ade604f2965f1f
Luis Pintado <luchopintado@gmail.com>
2017-04-19 18:45:00 -0500

    code alignement of precipitaciones.jsx

--- src/components/visortematicas/ambiental/precipitaciones.jsx
+++ src/components/visortematicas/ambiental/precipitaciones.jsx
@@ -29,20 +29,21 @@ export default class Precipitaciones extends React.Component{
 	constructor(props){
 		super(props);
 		this.state={
-			data:[['unidad', 'Por Dia'],
-        [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
-        [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
-        [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-        [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-        [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-        [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-        [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-        [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-        [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-        [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-        [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-        [66, 70], [67, 72], [68, 75], [69, 80]
-      ]
+			data:[
+                ['unidad', 'Por Dia'],
+                [0, 0],   [1, 10],  [2, 23],  [3, 17],  [4, 18],  [5, 9],
+                [6, 11],  [7, 27],  [8, 33],  [9, 40],  [10, 32], [11, 35],
+                [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
+                [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
+                [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
+                [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
+                [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
+                [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
+                [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
+                [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
+                [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
+                [66, 70], [67, 72], [68, 75], [69, 80]
+            ]
 		}
 	}
 	handleChange(key, event) {
@@ -63,145 +64,140 @@ export default class Precipitaciones extends React.Component{
 
 			<div className="tematica-home">
 				<AppBar
-						title="Precipitación y Temperatura"
-						iconElementLeft={<IconButton href="#/tematica/-KhDkIXgXKSWQpblXLLk/stats"><FontIcon  className="material-icons" >arrow_back</FontIcon></IconButton>}
-						style={style.appbar}
-						/>
-				
-
-	
+    				title="Precipitación y Temperatura"
+    				iconElementLeft={<IconButton href="#/tematica/-KhDkIXgXKSWQpblXLLk/stats"><FontIcon  className="material-icons" >arrow_back</FontIcon></IconButton>}
+    				style={style.appbar}
+				/>
 				<div className="col-md-12" className="tematica-home-container">
 					<div className={'my-pretty-chart-container'}>
 						<Chart
-						chartType="LineChart"
-						data={this.state.data}
-						options={{}}
-						graph_id="ScatterChart"
-						width="100%"
-						height="400px"
-						legend_toggle
+    						chartType="LineChart"
+    						data={this.state.data}
+    						options={{}}
+    						graph_id="ScatterChart"
+    						width="100%"
+    						height="400px"
+    						legend_toggle
 						/>
 					</div>
 					<div className="container-fluid">
 					<div className="row">
 						<div className="col-md-6">
 							<SelectField
-							fullWidth
-							floatingLabelText="Frequency"
-							value={this.state.value}
-							onChange={this.handleChange}
+    							fullWidth
+    							floatingLabelText="Frequency"
+    							value={this.state.value}
+    							onChange={this.handleChange}
 							>
-							<MenuItem value={1} primaryText="Never" />
-							<MenuItem value={2} primaryText="Every Night" />
-							<MenuItem value={3} primaryText="Weeknights" />
-							<MenuItem value={4} primaryText="Weekends" />
-							<MenuItem value={5} primaryText="Weekly" />
+    							<MenuItem value={1} primaryText="Never" />
+    							<MenuItem value={2} primaryText="Every Night" />
+    							<MenuItem value={3} primaryText="Weeknights" />
+    							<MenuItem value={4} primaryText="Weekends" />
+    							<MenuItem value={5} primaryText="Weekly" />
 							</SelectField>
 						</div>
 						<div className="col-md-6">
 							<SelectField
-									fullWidth
-									floatingLabelText="Frequency"
-									value={this.state.value}
-									onChange={this.handleChange}
-									>
-									<MenuItem value={1} primaryText="Never" />
-									<MenuItem value={2} primaryText="Every Night" />
-									<MenuItem value={3} primaryText="Weeknights" />
-									<MenuItem value={4} primaryText="Weekends" />
-									<MenuItem value={5} primaryText="Weekly" />
-							</SelectField>
-						</div>
-						<div className="col-md-6">
-							<SelectField
-							fullWidth
-							floatingLabelText="Frequency"
-							value={this.state.value}
-							onChange={this.handleChange}
+    							fullWidth
+    							floatingLabelText="Frequency"
+    							value={this.state.value}
+    							onChange={this.handleChange}
 							>
-							<MenuItem value={1} primaryText="Never" />
-							<MenuItem value={2} primaryText="Every Night" />
-							<MenuItem value={3} primaryText="Weeknights" />
-							<MenuItem value={4} primaryText="Weekends" />
-							<MenuItem value={5} primaryText="Weekly" />
+    							<MenuItem value={1} primaryText="Never" />
+    							<MenuItem value={2} primaryText="Every Night" />
+    							<MenuItem value={3} primaryText="Weeknights" />
+    							<MenuItem value={4} primaryText="Weekends" />
+    							<MenuItem value={5} primaryText="Weekly" />
 							</SelectField>
 						</div>
 						<div className="col-md-6">
 							<SelectField
-									fullWidth
-									floatingLabelText="Frequency"
-									value={this.state.value}
-									onChange={this.handleChange}
-									>
-									<MenuItem value={1} primaryText="Never" />
-									<MenuItem value={2} primaryText="Every Night" />
-									<MenuItem value={3} primaryText="Weeknights" />
-									<MenuItem value={4} primaryText="Weekends" />
-									<MenuItem value={5} primaryText="Weekly" />
-							</SelectField>
-						</div>
-						<div className="col-md-6">
-							<SelectField
-							fullWidth
-							floatingLabelText="Frequency"
-							value={this.state.value}
-							onChange={this.handleChange}
+    							fullWidth
+    							floatingLabelText="Frequency"
+    							value={this.state.value}
+    							onChange={this.handleChange}
 							>
-							<MenuItem value={1} primaryText="Never" />
-							<MenuItem value={2} primaryText="Every Night" />
-							<MenuItem value={3} primaryText="Weeknights" />
-							<MenuItem value={4} primaryText="Weekends" />
-							<MenuItem value={5} primaryText="Weekly" />
+    							<MenuItem value={1} primaryText="Never" />
+    							<MenuItem value={2} primaryText="Every Night" />
+    							<MenuItem value={3} primaryText="Weeknights" />
+    							<MenuItem value={4} primaryText="Weekends" />
+    							<MenuItem value={5} primaryText="Weekly" />
 							</SelectField>
 						</div>
 						<div className="col-md-6">
 							<SelectField
-									fullWidth
-									floatingLabelText="Frequency"
-									value={this.state.value}
-									onChange={this.handleChange}
-									>
-									<MenuItem value={1} primaryText="Never" />
-									<MenuItem value={2} primaryText="Every Night" />
-									<MenuItem value={3} primaryText="Weeknights" />
-									<MenuItem value={4} primaryText="Weekends" />
-									<MenuItem value={5} primaryText="Weekly" />
-							</SelectField>
-						</div>
-						<div className="col-md-6">
-							<SelectField
-							fullWidth
-							floatingLabelText="Frequency"
-							value={this.state.value}
-							onChange={this.handleChange}
+    							fullWidth
+    							floatingLabelText="Frequency"
+    							value={this.state.value}
+    							onChange={this.handleChange}
 							>
-							<MenuItem value={1} primaryText="Never" />
-							<MenuItem value={2} primaryText="Every Night" />
-							<MenuItem value={3} primaryText="Weeknights" />
-							<MenuItem value={4} primaryText="Weekends" />
-							<MenuItem value={5} primaryText="Weekly" />
+    							<MenuItem value={1} primaryText="Never" />
+    							<MenuItem value={2} primaryText="Every Night" />
+    							<MenuItem value={3} primaryText="Weeknights" />
+    							<MenuItem value={4} primaryText="Weekends" />
+    							<MenuItem value={5} primaryText="Weekly" />
 							</SelectField>
 						</div>
 						<div className="col-md-6">
 							<SelectField
-									fullWidth
-									floatingLabelText="Frequency"
-									value={this.state.value}
-									onChange={this.handleChange}
-									>
-									<MenuItem value={1} primaryText="Never" />
-									<MenuItem value={2} primaryText="Every Night" />
-									<MenuItem value={3} primaryText="Weeknights" />
-									<MenuItem value={4} primaryText="Weekends" />
-									<MenuItem value={5} primaryText="Weekly" />
+    							fullWidth
+    							floatingLabelText="Frequency"
+    							value={this.state.value}
+    							onChange={this.handleChange}
+							>
+    							<MenuItem value={1} primaryText="Never" />
+    							<MenuItem value={2} primaryText="Every Night" />
+    							<MenuItem value={3} primaryText="Weeknights" />
+    							<MenuItem value={4} primaryText="Weekends" />
+    							<MenuItem value={5} primaryText="Weekly" />
+							</SelectField>
+						</div>
+						<div className="col-md-6">
+							<SelectField
+								fullWidth
+								floatingLabelText="Frequency"
+								value={this.state.value}
+								onChange={this.handleChange}
+							>
+								<MenuItem value={1} primaryText="Never" />
+								<MenuItem value={2} primaryText="Every Night" />
+								<MenuItem value={3} primaryText="Weeknights" />
+								<MenuItem value={4} primaryText="Weekends" />
+								<MenuItem value={5} primaryText="Weekly" />
+							</SelectField>
+						</div>
+						<div className="col-md-6">
+							<SelectField
+    							fullWidth
+    							floatingLabelText="Frequency"
+    							value={this.state.value}
+    							onChange={this.handleChange}
+							>
+    							<MenuItem value={1} primaryText="Never" />
+    							<MenuItem value={2} primaryText="Every Night" />
+    							<MenuItem value={3} primaryText="Weeknights" />
+    							<MenuItem value={4} primaryText="Weekends" />
+    							<MenuItem value={5} primaryText="Weekly" />
+							</SelectField>
+						</div>
+						<div className="col-md-6">
+							<SelectField
+								fullWidth
+								floatingLabelText="Frequency"
+								value={this.state.value}
+								onChange={this.handleChange}
+							>
+								<MenuItem value={1} primaryText="Never" />
+								<MenuItem value={2} primaryText="Every Night" />
+								<MenuItem value={3} primaryText="Weeknights" />
+								<MenuItem value={4} primaryText="Weekends" />
+								<MenuItem value={5} primaryText="Weekly" />
 							</SelectField>
 						</div>
 					</div>
 					</div>
 				</div>
 			</div>
-
-
 		);
 	}
 }
