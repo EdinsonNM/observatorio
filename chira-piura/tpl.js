eb697c1a0b3c3e3188bc6090938c78bcc9a1693a
Edinson Nuñez More <nmedinson@gmail.com>
2017-07-12 10:54:18 -0500

    update 12-07-2017

--- chira-piura/tpl.js
+++ chira-piura/tpl.js
@@ -63,12 +63,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 513);
+/******/ 	return __webpack_require__(__webpack_require__.s = 514);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 513:
+/***/ 514:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
